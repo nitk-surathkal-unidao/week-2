0a0d569b395c693bc7ba0f7f22f2b36eb35743b2
Sahil kumar <supersen14@gmail.com>
2024-09-15 19:52:28 +0530

    contract completed

--- DeadmansSwitch.sol
+++ DeadmansSwitch.sol
@@ -4,29 +4,42 @@ pragma solidity ^0.8.0;
 contract DeadmansSwitch {
     // TODO: Declare state variables
     // Hint: You'll need variables for the owner, beneficiary, and last check-in block
+    address owner;
+    address beneficiary;
+    uint check_in_block;
 
     // TODO: Implement constructor
     constructor(address _beneficiary) {
         // Hint: Initialize state variables
+        owner=msg.sender;
+        beneficiary=_beneficiary;
     }
 
     // TODO: Implement still_alive function
     function still_alive() public {
         // Hint: Update the last check-in block
+        check_in_block=block.number;
     }
 
     // TODO: Implement release funds function
-    function releaseFunds() public {
+    function releaseFunds() public payable{
         // Hint: Check if 10 blocks have passed since last check-in
+        require(block.number>=10+check_in_block, "10 blocks has not passed");
+        uint256 balance = address(this).balance;
+        (bool success,)=beneficiary.call{value: balance}("");
+        require(success,"Transaction failed");
         // If so, transfer the contract balance to the beneficiary
     }
 
     // TODO: Implement receive function to allow the contract to receive Ether
-    receive() external payable {}
+    receive() external payable {
+       
+    }
 
     // Helper function for testing (optional)
     function getLastCheckInBlock() public view returns (uint256) {
         // TODO: Return the last check-in block
+        return check_in_block;
     }
 
     // Helper function for testing (optional)
@@ -34,3 +47,5 @@ contract DeadmansSwitch {
         return block.number;
     }
 }
+
+//address of contract on sepolia faucet testnet=0xec766fE4cf66646BE1D02E98E8BD6105cAe41F52
