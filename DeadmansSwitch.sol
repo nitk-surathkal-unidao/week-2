ffd292a7129b5ecdb79407113fe89d072ccac11a
Akshat-Bharara <akshat.bharara05@gmail.com>
2024-09-26 15:41:40 +0530

    Week2 Solution - Implement DeadmansSwitch

--- DeadmansSwitch.sol
+++ DeadmansSwitch.sol
@@ -4,21 +4,31 @@ pragma solidity ^0.8.0;
 contract DeadmansSwitch {
     // TODO: Declare state variables
     // Hint: You'll need variables for the owner, beneficiary, and last check-in block
+    address public owner;
+    address public beneficiary;
+    uint256 public lastCheckIn; // the functions ahead return uint256
 
     // TODO: Implement constructor
     constructor(address _beneficiary) {
         // Hint: Initialize state variables
+        owner = msg.sender;
+        beneficiary = _beneficiary;
+        lastCheckIn = block.number;
     }
 
     // TODO: Implement still_alive function
     function still_alive() public {
         // Hint: Update the last check-in block
+        require(msg.sender == owner);
+        lastCheckIn = block.number;
     }
 
     // TODO: Implement release funds function
     function releaseFunds() public {
         // Hint: Check if 10 blocks have passed since last check-in
         // If so, transfer the contract balance to the beneficiary
+        require(block.number > lastCheckIn + 10);
+        payable(beneficiary).transfer(address(this).balance);
     }
 
     // TODO: Implement receive function to allow the contract to receive Ether
@@ -27,6 +37,7 @@ contract DeadmansSwitch {
     // Helper function for testing (optional)
     function getLastCheckInBlock() public view returns (uint256) {
         // TODO: Return the last check-in block
+        return lastCheckIn;
     }
 
     // Helper function for testing (optional)
