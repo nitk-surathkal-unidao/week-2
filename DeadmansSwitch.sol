539e28e7243df81f140b3d5cae9f0bbc73a07c6d
Vedant Tarale <vedanttarale10@gmail.com>
2024-09-27 23:18:07 +0530

    Update DeadmansSwitch.sol

--- DeadmansSwitch.sol
+++ DeadmansSwitch.sol
@@ -1,32 +1,32 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
+import "./Owner.sol";
 
-contract DeadmansSwitch {
-    // TODO: Declare state variables
-    // Hint: You'll need variables for the owner, beneficiary, and last check-in block
+contract DeadmansSwitch is Owner {
+    address beneficiary;
+    uint256 lastCheckInBlock;
 
-    // TODO: Implement constructor
     constructor(address _beneficiary) {
-        // Hint: Initialize state variables
+        beneficiary = _beneficiary;
+        lastCheckInBlock = block.number;
     }
 
-    // TODO: Implement still_alive function
-    function still_alive() public {
-        // Hint: Update the last check-in block
+    function still_alive() public isOwner{
+        lastCheckInBlock = block.number;
     }
 
     // TODO: Implement release funds function
     function releaseFunds() public {
-        // Hint: Check if 10 blocks have passed since last check-in
-        // If so, transfer the contract balance to the beneficiary
+        require(block.number >= lastCheckInBlock + 10, "10 blocks have not passed yet");
+        payable(beneficiary).transfer(address(this).balance);
     }
 
-    // TODO: Implement receive function to allow the contract to receive Ether
     receive() external payable {}
 
     // Helper function for testing (optional)
     function getLastCheckInBlock() public view returns (uint256) {
-        // TODO: Return the last check-in block
+        return lastCheckInBlock;
     }
 
     // Helper function for testing (optional)
