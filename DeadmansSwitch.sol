b664c0b151ab76145ab34078c4d763406b3879ae
mallikaakash <mallik.aakash777@gmail.com>
2024-09-27 21:50:12 +0530

    solution

--- DeadmansSwitch.sol
+++ DeadmansSwitch.sol
@@ -5,20 +5,31 @@ contract DeadmansSwitch {
     // TODO: Declare state variables
     // Hint: You'll need variables for the owner, beneficiary, and last check-in block
 
+    address public owner;
+    address public beneficiary;
+    uint256 public lastCheckInBlock;
+    uint256 public constant BLOCKS_BEFORE_RELEASE = 10;
+
+
     // TODO: Implement constructor
     constructor(address _beneficiary) {
-        // Hint: Initialize state variables
+        owner = msg.sender;
+        beneficiary = _beneficiary;
+        lastCheckInBlock = block.number;
     }
 
     // TODO: Implement still_alive function
     function still_alive() public {
-        // Hint: Update the last check-in block
+        require(msg.sender == owner, "Only owner can check in");
+        lastCheckInBlock = block.number;
     }
 
     // TODO: Implement release funds function
     function releaseFunds() public {
         // Hint: Check if 10 blocks have passed since last check-in
         // If so, transfer the contract balance to the beneficiary
+        require(block.number > lastCheckInBlock + BLOCKS_BEFORE_RELEASE, "Too early to release funds");
+        payable(beneficiary).transfer(address(this).balance);
     }
 
     // TODO: Implement receive function to allow the contract to receive Ether
@@ -26,7 +37,7 @@ contract DeadmansSwitch {
 
     // Helper function for testing (optional)
     function getLastCheckInBlock() public view returns (uint256) {
-        // TODO: Return the last check-in block
+        return lastCheckInBlock;
     }
 
     // Helper function for testing (optional)
